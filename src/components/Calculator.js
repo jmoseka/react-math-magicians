d3f850c009473bb071edf7bfc393575c1a049f8c
jmoseka <jamylam3@gmail.com>
2022-05-26 15:34:03 +0300

    Add event

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,50 +1,69 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import React from 'react';
 import '../css/Calculator.css';
+import calculate from '../logic/calculate';
 
-const Calculator = () => (
-  <div className="body">
-    <div className="container col-10 col-md-4">
-      <div className="row">
-        <div className="col-12 screen-output">
-          0
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.btnClick = this.btnClick.bind(this);
+    this.state = {};
+  }
+
+  btnClick(event) {
+    this.setState((state) => calculate(state, event.target.innerText));
+  }
+
+  render() {
+    const { next, total } = this.state;
+    return (
+      <div className="body">
+        <div className="container col-10 col-md-4">
+          <div className="row">
+            <div className="col-12 screen-output">
+              {next || total}
+            </div>
+          </div>
+
+          <div className="row">
+            <button onClick={this.btnClick} type="button" className="col-3">AC</button>
+            <button onClick={this.btnClick} type="button" className="col-3">/</button>
+            <button onClick={this.btnClick} type="button" className="col-3">%</button>
+            <button onClick={this.btnClick} type="button" className="col-3">+</button>
+          </div>
+
+          <div className="row">
+            <button onClick={this.btnClick} type="button" className="col-3">7</button>
+            <button onClick={this.btnClick} type="button" className="col-3">8</button>
+            <button onClick={this.btnClick} type="button" className="col-3">9</button>
+            <button onClick={this.btnClick} type="button" className="col-3">x</button>
+          </div>
+
+          <div className="row">
+            <button onClick={this.btnClick} type="button" className="col-3">4</button>
+            <button onClick={this.btnClick} type="button" className="col-3">5</button>
+            <button onClick={this.btnClick} type="button" className="col-3">6</button>
+            <button onClick={this.btnClick} type="button" className="col-3">-</button>
+          </div>
+
+          <div className="row">
+            <button onClick={this.btnClick} type="button" className="col-3">1</button>
+            <button onClick={this.btnClick} type="button" className="col-3">2</button>
+            <button onClick={this.btnClick} type="button" className="col-3">3</button>
+            <button onClick={this.btnClick} type="button" className="col-3">+</button>
+          </div>
+
+          <div className="row">
+            <button onClick={this.btnClick} type="button" className="col-9">0</button>
+            <button onClick={this.btnClick} type="button" className="col-3">=</button>
+          </div>
+
         </div>
       </div>
 
-      <div className="row">
-        <button type="button" className="col-3">AC</button>
-        <button type="button" className="col-3">/</button>
-        <button type="button" className="col-3">%</button>
-        <button type="button" className="col-3">+</button>
-      </div>
+    );
+  }
+}
 
-      <div className="row">
-        <button type="button" className="col-3">7</button>
-        <button type="button" className="col-3">8</button>
-        <button type="button" className="col-3">9</button>
-        <button type="button" className="col-3">x</button>
-      </div>
-
-      <div className="row">
-        <button type="button" className="col-3">4</button>
-        <button type="button" className="col-3">5</button>
-        <button type="button" className="col-3">6</button>
-        <button type="button" className="col-3">-</button>
-      </div>
-
-      <div className="row">
-        <button type="button" className="col-3">1</button>
-        <button type="button" className="col-3">2</button>
-        <button type="button" className="col-3">3</button>
-        <button type="button" className="col-3">+</button>
-      </div>
-
-      <div className="row">
-        <button type="button" className="col-9">0</button>
-        <button type="button" className="col-3">=</button>
-      </div>
-
-    </div>
-  </div>
-);
 export default Calculator;
